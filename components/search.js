033698bf5143340ab44680a0df1391023978f740
Vince Zuño <vincezun@yahoo.com>
2019-08-06 19:48:46 +0800

    Changed to class components

--- components/search.js
+++ components/search.js
@@ -1,47 +1,135 @@
-import { useState } from 'react';
+import { useState, useContext, Component } from 'react';
+import MovieProvider from './movieContext';
+import fetch from 'isomorphic-unfetch';
 import axios from 'axios';
+import Link from 'next/link';
 
 import '../static/styles/search.scss';
 
-const IndexPage = () => {
-  const [movies, setMovie] = useState([]);
+class Search extends Component {
+  constructor(props) {
+    super(props);
 
-  const performSearch = async searchValue => {
+    this.state = {
+      movies: []
+    };
+  }
+
+  performSearch = async searchValue => {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${
       process.env.API_KEY
     }&query=${searchValue}`;
 
-    try {
-      searchValue
-        ? await axios.get(url).then(movies => setMovie(movies.data.results))
-        : setMovie([]);
-    } catch (error) {
-      console.log(error);
+    if (searchValue) {
+      const res = await fetch(url);
+      const data = await res.json();
+      return this.setState({ movies: data.results });
+    } else {
+      this.setState({ movies: [] });
     }
   };
 
-  const handleChange = e => {
+  handleChange = e => {
     let value = e.target.value;
-    performSearch(value);
+    this.performSearch(value);
   };
 
-  return (
-    <>
-      <input
-        type='search'
-        name='search'
-        className='search-box'
-        onChange={handleChange}
-        placeholder='Search any movies'
-        autoComplete='off'
-      />
-      <ul className='search-overlay'>
-        {movies.slice(0, 8).map((movie, i) => (
-          <li key={i}>{movie.title}</li>
-        ))}
-      </ul>
-    </>
-  );
-};
+  render() {
+    return (
+      <>
+        <input
+          type='search'
+          name='search'
+          className='search-box'
+          onChange={this.handleChange}
+          placeholder='Search any movies'
+          autoComplete='off'
+        />
+        <ul className='search-overlay'>
+          {this.state.movies.slice(0, 8).map((movie, i) => (
+            <li key={i}>
+              <Link href='/movie/[id]' as={`/movie/${movie.id}`}>
+                <a
+                  onClick={() => (
+                    <MovieProvider
+                      value={{
+                        title: movie.title,
+                        poster: movie.poster_path,
+                        overview: movie.overview,
+                        releaseDate: movie.release_date,
+                        rating: movie.vote_average
+                      }}
+                    />
+                  )}
+                >
+                  {movie.title}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
 
-export default IndexPage;
+// const Search = () => {
+//   const [movieData, setMovieData] = useContext(MovieContext);
+
+//   const [movies, setMovie] = useState([]);
+
+//   const performSearch = async searchValue => {
+//     const url = `https://api.themoviedb.org/3/search/movie?api_key=${
+//       process.env.API_KEY
+//     }&query=${searchValue}`;
+
+//     try {
+//       searchValue
+//         ? await axios.get(url).then(movies => setMovie(movies.data.results))
+//         : setMovie([]);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   const handleChange = e => {
+//     let value = e.target.value;
+//     performSearch(value);
+//   };
+
+//   return (
+//     <>
+//       <input
+//         type='search'
+//         name='search'
+//         className='search-box'
+//         onChange={handleChange}
+//         placeholder='Search any movies'
+//         autoComplete='off'
+//       />
+//       <ul className='search-overlay'>
+//         {movies.slice(0, 8).map((movie, i) => (
+//           <li key={i}>
+//             <Link href='/movie/[id]' as={`/movie/${movie.id}`}>
+//               <a
+//                 onClick={() =>
+//                   setMovieData({
+//                     title: movie.title,
+//                     poster: movie.poster_path,
+//                     overview: movie.overview,
+//                     releaseDate: movie.release_date,
+//                     rating: movie.vote_average
+//                   })
+//                 }
+//               >
+//                 {movie.title}
+//               </a>
+//             </Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
+
+export default Search;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import fetch from 'isomorphic-unfetch';
 
 import Layout from '../components/layout/layout';
 import Search from '../components/search';
@@ -8,107 +7,86 @@ import star from '../static/images/star.svg';
 
 import '../static/styles/index.scss';
 
-const Index = () => {
-  const [topRatedMovies, setTopRatedMovies] = useState([]);
-  const [popularMovies, setPopularMovies] = useState([]);
-  const [upcomingMovies, setUpcomingMovies] = useState([]);
-
-  useEffect(() => {
-    const topRatedURL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${
-      process.env.API_KEY
-    }`;
-
-    try {
-      axios
-        .get(topRatedURL)
-        .then(topRatedMovies => setTopRatedMovies(topRatedMovies.data.results));
-    } catch (error) {
-      console.log(error);
-    }
-
-    const popularURL = `https://api.themoviedb.org/3/movie/popular?api_key=${
-      process.env.API_KEY
-    }`;
-
-    try {
-      axios
-        .get(popularURL)
-        .then(popularMovies => setPopularMovies(popularMovies.data.results));
-    } catch (error) {
-      console.log(error);
-    }
-
-    const upcomingURL = `https://api.themoviedb.org/3/movie/upcoming?api_key=${
-      process.env.API_KEY
-    }`;
-
-    try {
-      axios
-        .get(upcomingURL)
-        .then(upcomingMovies => setUpcomingMovies(upcomingMovies.data.results));
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-
-  return (
-    <Layout>
-      <div className='home-w'>
-        <Search />
-        <div className='top-rated-w'>
-          <h2 className='heading'>Top Rated Movies</h2>
-          {topRatedMovies.slice(0, 4).map((topRatedMovie, i) => (
-            <figure key={i} className='movie'>
-              <img
-                src={`http://image.tmdb.org/t/p/w342${
-                  topRatedMovie.poster_path
-                }`}
-                alt={topRatedMovie.title}
-                className='poster'
-              />
-              <figcaption className='title'>{topRatedMovie.title}</figcaption>
-              <img src={star} alt='Star' className='star-icon' />
-              <p className='rating'>{topRatedMovie.vote_average}</p>
-            </figure>
-          ))}
-        </div>
-        <div className='popular-w'>
-          <h2 className='heading'>Popular Movies</h2>
-          {popularMovies.slice(0, 4).map((popularMovie, i) => (
-            <figure key={i} className='movie'>
-              <img
-                src={`http://image.tmdb.org/t/p/w342${
-                  popularMovie.poster_path
-                }`}
-                alt={popularMovie.title}
-                className='poster'
-              />
-              <figcaption className='title'>{popularMovie.title}</figcaption>
-              <img src={star} alt='Star' className='star-icon' />
-              <p className='rating'>{popularMovie.vote_average}</p>
-            </figure>
-          ))}
-        </div>
-        <div className='upcoming-w'>
-          <h2 className='heading'>Upcoming Movies</h2>
-          {upcomingMovies.slice(0, 4).map((upcomingMovie, i) => (
-            <figure key={i} className='movie'>
-              <img
-                src={`http://image.tmdb.org/t/p/w342${
-                  upcomingMovie.poster_path
-                }`}
-                alt={upcomingMovie.title}
-                className='poster'
-              />
-              <figcaption className='title'>{upcomingMovie.title}</figcaption>
-              <img src={star} alt='Star' className='star-icon' />
-              <p className='rating'>{upcomingMovie.vote_average}</p>
-            </figure>
-          ))}
-        </div>
+const Index = ({ topRatedMovies, popularMovies, upcomingMovies }) => (
+  <Layout>
+    <div className='home-w'>
+      <Search />
+      <div className='top-rated-w'>
+        <h2 className='heading'>Top Rated Movies</h2>
+        {topRatedMovies.slice(0, 4).map((topRatedMovie, i) => (
+          <figure key={i} className='movie'>
+            <img
+              src={`http://image.tmdb.org/t/p/w342${topRatedMovie.poster_path}`}
+              alt={topRatedMovie.title}
+              className='poster'
+            />
+            <figcaption className='title'>{topRatedMovie.title}</figcaption>
+            <img src={star} alt='Star' className='star-icon' />
+            <p className='rating'>{topRatedMovie.vote_average}</p>
+          </figure>
+        ))}
       </div>
-    </Layout>
-  );
+      <div className='popular-w'>
+        <h2 className='heading'>Popular Movies</h2>
+        {popularMovies.slice(0, 4).map((popularMovie, i) => (
+          <figure key={i} className='movie'>
+            <img
+              src={`http://image.tmdb.org/t/p/w342${popularMovie.poster_path}`}
+              alt={popularMovie.title}
+              className='poster'
+            />
+            <figcaption className='title'>{popularMovie.title}</figcaption>
+            <img src={star} alt='Star' className='star-icon' />
+            <p className='rating'>{popularMovie.vote_average}</p>
+          </figure>
+        ))}
+      </div>
+      <div className='upcoming-w'>
+        <h2 className='heading'>Upcoming Movies</h2>
+        {upcomingMovies.slice(0, 4).map((upcomingMovie, i) => (
+          <figure key={i} className='movie'>
+            <img
+              src={`http://image.tmdb.org/t/p/w342${upcomingMovie.poster_path}`}
+              alt={upcomingMovie.title}
+              className='poster'
+            />
+            <figcaption className='title'>{upcomingMovie.title}</figcaption>
+            <img src={star} alt='Star' className='star-icon' />
+            <p className='rating'>{upcomingMovie.vote_average}</p>
+          </figure>
+        ))}
+      </div>
+    </div>
+  </Layout>
+);
+
+Index.getInitialProps = async () => {
+  const topRatedURL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${
+    process.env.API_KEY
+  }`;
+
+  const popularURL = `https://api.themoviedb.org/3/movie/popular?api_key=${
+    process.env.API_KEY
+  }`;
+
+  const upcomingURL = `https://api.themoviedb.org/3/movie/upcoming?api_key=${
+    process.env.API_KEY
+  }`;
+
+  const resTopRated = await fetch(topRatedURL);
+  const dataTopRated = await resTopRated.json();
+
+  const resPopular = await fetch(popularURL);
+  const dataPopular = await resPopular.json();
+
+  const resUpcoming = await fetch(upcomingURL);
+  const dataUpcoming = await resUpcoming.json();
+
+  return {
+    topRatedMovies: dataTopRated.results,
+    popularMovies: dataPopular.results,
+    upcomingMovies: dataUpcoming.results
+  };
 };
 
 export default Index;
