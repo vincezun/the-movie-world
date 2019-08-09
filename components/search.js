5c3b9cd89a28a31a9ab5fdbc7db24ffb91a964c6
Vince Zuño <vincezun@yahoo.com>
2019-08-10 00:02:12 +0800

    Changed to functional components.

--- components/search.js
+++ components/search.js
@@ -1,18 +1,22 @@
+import { useState, useEffect } from 'react';
 import fetch from 'isomorphic-unfetch';
 import Link from 'next/link';
+import { useComponentVisible } from '../components/hook/useComponentVisible';
 
 import '../static/styles/search.scss';
 
-class Search extends React.Component {
-  constructor(props) {
-    super(props);
+import searchIcon from '../static/images/search-icon.svg';
 
-    this.state = {
-      movies: []
-    };
-  }
+const Search = () => {
+  const [movies, setMovies] = useState([]);
 
-  performSearch = async searchValue => {
+  const {
+    ref,
+    isComponentVisible,
+    setIsComponentVisible
+  } = useComponentVisible(true);
+
+  const performSearch = async searchValue => {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${
       process.env.API_KEY
     }&query=${searchValue}`;
@@ -20,118 +24,71 @@ class Search extends React.Component {
     if (searchValue) {
       const res = await fetch(url);
       const data = await res.json();
-      return this.setState({ movies: data.results });
+      return setMovies(data.results);
     } else {
-      this.setState({ movies: [] });
+      setMovies([]);
     }
   };
 
-  handleChange = e => {
+  const handleChange = e => {
     let value = e.target.value;
-    this.performSearch(value);
+    performSearch(value);
+    setIsComponentVisible(true);
+    const noResults = document.querySelector('.no-results');
+    noResults
+      ? value
+        ? noResults.classList.add('active')
+        : noResults.classList.remove('active')
+      : null;
   };
 
-  render() {
-    return (
-      <>
+  const hideOverlay = () => {
+    const searchBox = document.querySelector('.search-box');
+    searchBox.value = '';
+    setIsComponentVisible(false);
+  };
+
+  return (
+    <div ref={ref}>
+      <div className='search-w'>
         <input
           type='search'
           name='search'
           className='search-box'
-          onChange={this.handleChange}
+          onChange={handleChange}
+          onClick={handleChange}
           placeholder='Search any movies'
           autoComplete='off'
+          maxLength='75'
         />
+        <button className='search-btn'>
+          <img src={searchIcon} alt='Search' />
+        </button>
+      </div>
+      {isComponentVisible && (
         <ul className='search-overlay'>
-          {this.state.movies.slice(0, 8).map((movie, i) => (
-            <li key={i}>
-              <Link
-                href={{
-                  pathname: '/movie/[id]',
-                  query: {
-                    title: movie.title,
-                    poster: movie.poster_path,
-                    overview: movie.overview,
-                    releaseDate: movie.release_date,
-                    rating: movie.vote_average
-                  }
-                }}
-                as={`/movie/${movie.id}`}
-              >
-                <a
-                  onClick={() => {
-                    localStorage.setItem('title', movie.title);
-                    localStorage.setItem('poster', movie.poster_path);
-                    localStorage.setItem('overview', movie.overview);
-                    localStorage.setItem('releaseDate', movie.release_date);
-                    localStorage.setItem('rating', movie.vote_average);
-                  }}
-                >
-                  {movie.title}
-                </a>
-              </Link>
-            </li>
-          ))}
+          {movies !== undefined && movies.length > 0 ? (
+            movies.slice(0, 8).map((movie, i) => {
+              return (
+                <li key={i} onClick={hideOverlay}>
+                  <Link
+                    href={{
+                      pathname: '/search/[title]'
+                    }}
+                    as={`/search/${encodeURIComponent(movie.title)}`} //if % is in the url before it fetch, it will throw URIError: URI malformed
+                  >
+                    <a className='links'>{movie.title}</a>
+                  </Link>
+                </li>
+              );
+            })
+          ) : (
+            <p className='no-results'>No Results</p>
+          )}
         </ul>
-      </>
-    );
-  }
-}
-
-// const Search = () => {
-//   const [movies, setMovie] = useState([]);
-//   const performSearch = async searchValue => {
-//     const url = `https://api.themoviedb.org/3/search/movie?api_key=${
-//       process.env.API_KEY
-//     }&query=${searchValue}`;
-
-//     try {
-//       searchValue
-//         ? await axios.get(url).then(movies => setMovie(movies.data.results))
-//         : setMovie([]);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
-//   const handleChange = e => {
-//     let value = e.target.value;
-//     performSearch(value);
-//   };
-
-//   return (
-//     <>
-//       <input
-//         type='search'
-//         name='search'
-//         className='search-box'
-//         onChange={handleChange}
-//         placeholder='Search any movies'
-//         autoComplete='off'
-//       />
-//       <ul className='search-overlay'>
-//         {movies.slice(0, 8).map((movie, i) => (
-//           <li key={i}>
-//             <Link
-//               href={{
-//                 pathname: '/movie/[id]',
-//                 query: {
-//                   title: movie.title,
-//                   poster: movie.poster_path,
-//                   overview: movie.overview,
-//                   releaseDate: movie.release_date,
-//                   rating: movie.vote_average
-//                 }
-//               }}
-//               as={`/movie/${movie.id}`}
-//             >
-//               <a>{movie.title}</a>
-//             </Link>
-//           </li>
-//         ))}
-//       </ul>
-//     </>
-//   );
-// };
+      )}
+    </div>
+  );
+};
 
 export default Search;
