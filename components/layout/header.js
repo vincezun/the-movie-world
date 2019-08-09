7693c0624fd183a7b154e4c894cc1067d18c5c3f
Vince Zuño <vincezun@yahoo.com>
2019-08-09 23:59:18 +0800

    Removed logo in header.

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,40 +1,41 @@
 import Link from 'next/link';
 
-import logo from '../../static/images/logo.svg';
+import Search from '../search';
 
 import '../../static/styles/header.scss';
 
-const Header = () => (
-  <header className='header'>
-    <div className='nav'>
-      <div className='logo-w'>
-        <img src={logo} alt='Movie world logo' className='logo' />
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='nav'>
         <h1 className='heading'>Movie World</h1>
+        <span className='hamburger'>
+          <span className='bar' />
+        </span>
+        <Search />
       </div>
-      <span className='hamburger'>
-        <span className='bar' />
-      </span>
-    </div>
-    <nav className='menu'>
-      <ul className='overlay'>
-        <li>
-          <Link href='/movies/top-rated'>
-            <a>Top Rated</a>
-          </Link>
-        </li>
-        <li>
-          <Link href='/movies/popular'>
-            <a>Popular</a>
-          </Link>
-        </li>
-        <li>
-          <Link href='/movies/upcoming'>
-            <a>Upcoming</a>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+
+      <nav className='menu'>
+        <ul className='overlay'>
+          <li>
+            <Link href='/movies/top-rated'>
+              <a>Top Rated</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/movies/popular'>
+              <a>Popular</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/movies/upcoming'>
+              <a>Upcoming</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
