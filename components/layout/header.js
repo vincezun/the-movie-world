9194920eee13e6e15d991492ecac1373390999b4
Vince Zuño <vincezun@yahoo.com>
2019-08-14 20:41:24 +0800

    Added menu functionality in header.

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Link from 'next/link';
 
 import Search from '../search';
@@ -5,10 +6,21 @@ import Search from '../search';
 import '../../static/styles/header.scss';
 
 const Header = () => {
+  useEffect(() => {
+    const hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.menu');
+
+    hamburger.addEventListener('click', () => {
+      menu.classList.toggle('active');
+    });
+  }, []);
+
   return (
     <header className='header'>
       <div className='nav'>
-        <h1 className='heading'>Movie World</h1>
+        <Link href='/'>
+          <a className='heading'>Movie World</a>
+        </Link>
         <span className='hamburger'>
           <span className='bar' />
         </span>
@@ -18,17 +30,26 @@ const Header = () => {
       <nav className='menu'>
         <ul className='overlay'>
           <li>
-            <Link href='/movies/top-rated'>
+            <Link
+              href={{ pathname: '/top-rated/page/[number]' }}
+              as='/top-rated/page/1'
+            >
               <a>Top Rated</a>
             </Link>
           </li>
           <li>
-            <Link href='/movies/popular'>
+            <Link
+              href={{ pathname: '/popular/page/[number]' }}
+              as='/popular/page/1'
+            >
               <a>Popular</a>
             </Link>
           </li>
           <li>
-            <Link href='/movies/upcoming'>
+            <Link
+              href={{ pathname: '/upcoming/page/[number]' }}
+              as='/upcoming/page/1'
+            >
               <a>Upcoming</a>
             </Link>
           </li>
