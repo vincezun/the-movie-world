17eda7f0d1150e994987656e3fa44798d3f86e5e
Vince Zuño <vincezun@yahoo.com>
2019-08-15 22:33:30 +0800

    Added link of every movie in movie-card.

--- components/movie/movie-card.js
+++ components/movie/movie-card.js
@@ -1,33 +1,44 @@
+import Link from 'next/link';
 import posterNotAvailabble from '../../static/images/poster-not-available.svg';
+import { useSlugify } from '../hooks/useSlugify';
 
 import '../../static/styles/movie-card.scss';
 
-const MovieCard = ({ poster, title, releaseDate }) => (
-  <figure className='movie'>
-    {poster ? (
-      <img
-        src={`http://image.tmdb.org/t/p/w370_and_h556_bestv2${poster}`}
-        alt={title}
-        className='poster'
-      />
-    ) : (
-      <img
-        src={posterNotAvailabble}
-        alt='Poster not available'
-        className='poster'
-      />
-    )}
-    {title ? (
-      <figcaption className='title'>{title}</figcaption>
-    ) : (
-      <figcaption className='title'>N/A</figcaption>
-    )}
-    {releaseDate ? (
-      <p className='release-date'>{releaseDate}</p>
-    ) : (
-      <p className='release-date'>N/A</p>
-    )}
-  </figure>
-);
+const MovieCard = ({ poster, title, releaseDate, id }) => {
+  return (
+    <Link
+      href={{ pathname: '/movie/[id]/[title]' }}
+      as={`/movie/${id}/${useSlugify(title)}`}
+    >
+      <a className='movie'>
+        <figure>
+          {poster ? (
+            <img
+              src={`http://image.tmdb.org/t/p/w370_and_h556_bestv2${poster}`}
+              alt={title}
+              className='poster'
+            />
+          ) : (
+            <img
+              src={posterNotAvailabble}
+              alt='Poster not available'
+              className='poster'
+            />
+          )}
+          {title ? (
+            <figcaption className='title'>{title}</figcaption>
+          ) : (
+            <figcaption className='title'>N/A</figcaption>
+          )}
+          {releaseDate ? (
+            <p className='release-date'>{releaseDate}</p>
+          ) : (
+            <p className='release-date'>N/A</p>
+          )}
+        </figure>
+      </a>
+    </Link>
+  );
+};
 
 export default MovieCard;
