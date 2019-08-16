0473b104b148907704f716dd07cece0b7f42df14
Vince Zuño <vincezun@yahoo.com>
2019-08-16 20:47:11 +0800

    Added stuff

---
+++ components/movie/movie-casts.js
@@ -0,0 +1,28 @@
+import profileNotAvailable from '../../static/images/poster-not-available.svg';
+
+import '../../static/styles/movie-casts.scss';
+
+const MovieCast = ({ name, character, profilePath }) => {
+  return (
+    <div className='cast'>
+      {profilePath ? (
+        <img
+          src={`http://image.tmdb.org/t/p/w45${profilePath}`}
+          alt={name}
+          className='profile'
+        />
+      ) : (
+        <img src={profileNotAvailable} alt={name} className='profile' />
+      )}
+      <p className='name'>{name}</p>
+      <span className='ellipsis'>...</span>
+      {character ? (
+        <p className='character'>{character}</p>
+      ) : (
+        <p className='character'>N/A</p>
+      )}
+    </div>
+  );
+};
+
+export default MovieCast;
---
+++ pages/movie/[id]/[title]/casts.js
@@ -0,0 +1,72 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import fetch from 'isomorphic-unfetch';
+
+import Layout from '../../../../components/layout/layout';
+import MovieCasts from '../../../../components/movie/movie-casts';
+
+import '../../../../static/styles/full-cast.scss';
+
+const FullMovieCast = ({ movieCasts, movie }) => {
+  const router = useRouter();
+  const { id, title } = router.query;
+
+  return (
+    <Layout>
+      <div className='full-cast-w'>
+        <ul className='links'>
+          <li>
+            <Link
+              href={{ pathname: '/movie/[id]/[title]' }}
+              as={`/movie/${id}/${title}`}
+            >
+              <a className='back'>&laquo; Back</a>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={{ pathname: '/movie/[id]/[title]' }}
+              as={`/movie/${id}/${title}`}
+            >
+              <a className='movie-heading'>{movie.title}</a>
+            </Link>
+          </li>
+        </ul>
+        <p className='cast-heading'>Full Cast</p>
+        {movieCasts.map((cast, i) => (
+          <MovieCasts
+            key={i}
+            profilePath={cast.profile_path}
+            name={cast.name}
+            character={cast.character}
+          />
+        ))}
+      </div>
+    </Layout>
+  );
+};
+
+FullMovieCast.getInitialProps = async context => {
+  const { id } = context.query;
+
+  const movieUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${
+    process.env.API_KEY
+  }`;
+
+  const castsUrl = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${
+    process.env.API_KEY
+  }`;
+
+  const movieRes = await fetch(movieUrl);
+  const movieData = await movieRes.json();
+
+  const castsRes = await fetch(castsUrl);
+  const castsData = await castsRes.json();
+
+  return {
+    movieCasts: castsData.cast,
+    movie: movieData
+  };
+};
+
+export default FullMovieCast;
